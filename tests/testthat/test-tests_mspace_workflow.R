7fb0c8449cb8106f9769d0a9724e058e11ef0842
millacarmona <pablomillac@gmail.com>
2023-08-29 22:17:16 +0100

    bh

--- tests/testthat/test-tests_mspace_workflow.R
+++ tests/testthat/test-tests_mspace_workflow.R
@@ -431,9 +431,16 @@ test_that(desc = "testing proj_groups, stacking behavior", code = {
   # tab <- geomorph::two.d.array(shapes)
   # index_x_in_sc <- as.numeric(unlist(apply(gr_scores, 1, function(x, y) {which(apply(y, 1, function(z, x){all(z == x)}, x))},stats::prcomp(tab)$x)))
 
+  # index_x_in_sc <- c(8:52, 75:148, 169:281, 1:7, 53:74, 149:168)
+  # result1 <- all(index_x_in_sc == c(which(!index), which(index)))
 
-  pca <- stats::prcomp(geomorph::two.d.array(shapes[,,1:dim(shapes)[3] %in% c(which(!index), which(index))]))
-  result1 <- all(msp1$projected$gr_scores == pca$x[c(which(!index), which(index)),])
+  gr_scores <- msp1$projected$gr_scores
+  x <- msp1$ordination$x
+  #pca <- stats::prcomp(geomorph::two.d.array(shapes[,,1:dim(shapes)[3] %in% c(which(!index), which(index))]))
+  #result1 <- all(gr_scores == x[c(which(!index), which(index)),])
+
+  result1 <- all(gr_scores == x[c(8:52, 75:148, 169:281, 1:7, 53:74, 149:168),])
+
 
   expect_true(all(result1))
   dev.off()
